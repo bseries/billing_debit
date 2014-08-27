1c5edfa9776611a1fd82c24346cc6bf39c2d4878
David Persson <davidpersson@gmx.de>
2014-08-27 17:43:27 +0200

    Adding schema.

---
+++ data/schema.sql
@@ -0,0 +1,23 @@
+-- Create syntax for TABLE 'billing_banks'
+CREATE TABLE `billing_banks` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `bic` varchar(50) NOT NULL DEFAULT '',
+  `name` varchar(250) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `bic` (`bic`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Create syntax for TABLE 'billing_debit_cards'
+CREATE TABLE `billing_debit_cards` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned DEFAULT NULL,
+  `virtual_user_id` int(11) unsigned DEFAULT NULL,
+  `holder` varchar(250) NOT NULL,
+  `iban` varchar(50) NOT NULL DEFAULT '' COMMENT 'length varies on countries 16-30 incl. buffer',
+  `bic` varchar(20) NOT NULL DEFAULT '' COMMENT 'length varies 8-11 incl. buffer',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `virtual_user_id` (`virtual_user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
